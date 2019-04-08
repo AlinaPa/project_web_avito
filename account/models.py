b25519878b1e6bcc4e95bcbfdf3988137605dfa4
AlinaPa <aliaska-92@mail.ru>
2019-04-08 12:32:03 +0300

    change account models.py and products models.py

--- account/models.py
+++ account/models.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
 from django.db import models
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(AbstractUser):
-    username = models.CharField(_('Имя пользователя'), max_length=30, unique=True, blank=True)
-    password = models.CharField(_('Пароль'), max_length=128, blank=True)
-    email = models.EmailField(_('Электронная почта'), unique=True)
+    username = models.CharField('Имя пользователя', max_length=30, unique=True, blank=True)
+    password = models.CharField('Пароль', max_length=128, blank=True)
+    email = models.EmailField('Электронная почта', unique=True)
 
     def __str__(self):
         return self.username or self.email
