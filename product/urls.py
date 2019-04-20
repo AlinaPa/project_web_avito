a509cba748e2eb6fe5ba442e47ad3eed724fc7e2
AlinaPa <aliaska-92@mail.ru>
2019-04-20 12:53:38 +0300

    added main_page

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from product.views import CreateProductView, ProductDetailView, ProductListView
+from product.views import CreateProductView, ProductDetailView
 
 urlpatterns = [
-    url(r'', ProductListView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='view_product'),
     url(r'^create/$', CreateProductView.as_view(), name='product_create'),
     url(r'^success_create/$', TemplateView.as_view(template_name='products/success_create.html'), name='success_create'),
