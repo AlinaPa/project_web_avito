8733cd118c6aa093212567a5957fcb096005ca10
AlinaPa <aliaska-92@mail.ru>
2019-06-17 17:37:36 +0300

    change admin account and forms account

--- account/forms.py
+++ account/forms.py
@@ -1,19 +1,21 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UsernameField, UserCreationForm
 
 from account.models import User
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(label='Почта пользователя', max_length=150)
-    password = forms.CharField(label='Пароль', max_length=8, widget=forms.PasswordInput)
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = (
+            'username',
             'email',
             'password1',
             'password2'
         )
+
