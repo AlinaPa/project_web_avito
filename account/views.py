a9244b1e39ffad874789dae6f82a6e93736d8ef2
AlinaPa <aliaska-92@mail.ru>
2019-06-17 17:39:26 +0300

    added Profile

--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
-from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from django.views import View
+from django.views.generic import FormView, TemplateView
 from django import forms
-from django.contrib.auth import authenticate, login
-
-from account.models import User
+from django.contrib.auth import authenticate, login, logout
 
 from account.forms import LoginForm, RegistrationForm
+from account.models import User, Profile
 
 
 class LoginView(FormView):
@@ -13,12 +14,18 @@ class LoginView(FormView):
     success_url = "/"
 
     def form_valid(self, form):
-        email_user = User.objects.filter(email__iexact=form.cleaned_data['email']).first()
-        if email_user:
-            user = authenticate(email=form.cleaned_data['email'], password=form.cleaned_data['password'])
+        username = User.objects.filter(username__iexact=form.cleaned_data['username']).first()
+        if username:
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             login(self.request, user)
             return super().form_valid(form)
         raise forms.ValidationError('Неверно указан email или пароль')
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect("/")
 
 
 class RegisterView(FormView):
@@ -29,3 +36,9 @@ class RegisterView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class ProfileView(TemplateView):
+    model = Profile
+    template_name = "account/profile.html"
+
